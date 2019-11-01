88ab3cee29b30e405ed4a61c94aca73fc4cc79a7
Luis del Amo <luis@salonkee.lu>
2019-11-01 17:24:29 +0100

    adding Single Responsability principle

---
+++ 01-single-responsability-principle-pattern.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+class ErrorHandler {
+  constructor(error) {
+    this.error = error;
+  }
+  static init(error) {
+    return new ErrorHandler(error);
+  }
+  handle() {
+    let error = this.error;
+    switch(error.code) {
+      case 400:
+          console.log(`Error with code ${error.code}, ${error.description}`);
+        break;
+      case 401:
+          console.log(`Error with code ${error.code}, ${error.description}`);
+        break;
+      case 500:
+          console.log(`Error with code ${error.code}, ${error.description}`);
+        break;
+      default:
+        console.log('Unknow error');
+        break;
+    }
+  }
+}
+
+class HttpClient {
+  get(url) {
+    return axios.get(url)
+      .then((response) => {
+         return response;
+      })
+      .catch(error => {
+        ErrorHandler.init(error.response.data).handle();
+      })
+  }
+}
+
+let httpClient = new HttpClient();
+
+httpClient.get('https://httpstat.us/500')
+  .then(response => {
+    return response;
+  })
